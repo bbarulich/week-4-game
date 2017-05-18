d5cf569f9a9fa0b19f1cf800476cb19a4d82ccbe
Brandon Barulich <BrandonBarulich@Brandons-MacBook-Pro.local>
2017-05-18 17:33:36 -0500

    added wins losses varibles and functionality etc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
-
+//defining varibles
 var randomnum=Math.floor(Math.random()*101+19)
-
-$('#toguess') .text(randomnum);
+var yourscore=0
+var wins=0
+var losses=0
+var gemone=Math.floor(Math.random()*11+1)
+var gemtwo=Math.floor(Math.random()*11+1)
+var gemthree=Math.floor(Math.random()*11+1)
+var gemfour=Math.floor(Math.random()*11+1)
+//console.log(gemfour)
+//assigning the random number function to the two guess div
+$('#toguess') .text("Number to Reach: "+randomnum);
+$('#numwin') .text("Wins: "+wins);
+$('#numloss').text("Losses: "+losses)
